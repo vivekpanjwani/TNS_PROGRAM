c05a7ca2fe56fed161bb60c45ab26a7db1ae1d5b
Vivek Panjwani <vickydpanjwani@outlook.com>
2023-10-13 14:24:46 +0530

    Initial Commit

---
+++ TNS_DAY_1/src/Exercise/Small_number.java
@@ -0,0 +1,16 @@
+package Exercise;
+
+
+public class Small_number {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num1 =1;
+		int num2 =2;
+		int num3 =3;
+		
+		System.out.println("The Smallest Number is:" + Math.min(Math.min(num2, num3),num1));
+		
+	}
+
+}
---
+++ TNS_DAY_1/src/Exercise/Sum_Mul_Div.java
@@ -0,0 +1,18 @@
+package Exercise;
+
+
+public class Sum_Mul_Div {
+
+	public static void main(String[] args) {
+		int a = 12;
+		int b = 0;
+		int sum = a+b;
+		int mul = a*b;
+		float div = (float) a/b;
+		
+		System.out.println("Sum Is " + sum);
+		System.out.println("Div Is " + div);
+		System.out.println("Mul is " + mul);
+	} 
+
+}
